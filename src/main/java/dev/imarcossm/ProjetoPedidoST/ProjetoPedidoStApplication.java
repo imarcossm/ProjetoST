e918d5ede86703342d99809f31b2a1d657438e15
imarcossm <viniciusmorais.com@gmail.com>
2026-02-19 22:16:02 -0300

    Configuração inicial da estrutura em spring boot do projeto

---
+++ src/main/java/dev/imarcossm/ProjetoPedidoST/ProjetoPedidoStApplication.java
@@ -0,0 +1,13 @@
+package dev.imarcossm.ProjetoPedidoST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPedidoStApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPedidoStApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/imarcossm/ProjetoPedidoST/ProjetoPedidoStApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.imarcossm.ProjetoPedidoST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPedidoStApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
